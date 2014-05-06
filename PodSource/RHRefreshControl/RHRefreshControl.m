de858d8fc833fce9bec4997b679ac20cdf6010c6
andris-zalitis <andris@Andriss-Mac-Pro.local>
2014-05-06 12:22:24 +0300

    Moved back to KVO, fixed the issues and all looks fine now

--- PodSource/RHRefreshControl/RHRefreshControl.m
+++ PodSource/RHRefreshControl/RHRefreshControl.m
@@ -27,6 +27,7 @@ static void * KVOContext = &KVOContext;
 {
     UIEdgeInsets _originalInsets;
     BOOL _ignoreInsetChanges;
+    BOOL _insetSet;
     
     __weak id<UIScrollViewDelegate> _realDelegate;
 }
@@ -45,7 +46,6 @@ static void * KVOContext = &KVOContext;
 - (void)attachToScrollView:(UIScrollView *)scrollView {
     _scrollView = scrollView;
     if ([scrollView isKindOfClass:[UITableView class]] && [(UITableView *)scrollView tableHeaderView]) {
-        //[scrollView insertSubview:self.refreshView atIndex:1];
         [[(UITableView *)scrollView tableHeaderView] addSubview:self.refreshView];
     } else {
         [scrollView insertSubview:self.refreshView atIndex:0];
@@ -54,25 +54,16 @@ static void * KVOContext = &KVOContext;
     _originalInsets = scrollView.contentInset;
     [self positionRefreshView];
     
-//    [scrollView addObserver:self forKeyPath:@"contentInset" options:NSKeyValueObservingOptionNew context:KVOContext];
-
-    [self setScrollViewDelegate:_scrollView.delegate];
+    [scrollView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:KVOContext];
+    [scrollView addObserver:self forKeyPath:@"contentInset" options:NSKeyValueObservingOptionNew context:KVOContext];
     
     _attached = YES;
 }
 
-// save the reference to the real scrollview delegate but attach to scrollview ourself
-- (void)setScrollViewDelegate:(id<UIScrollViewDelegate>)delegate
-{
-    _realDelegate = delegate;
-    _scrollView.delegate = self;
-    
-}
-
 - (void)dealloc
 {
-//    [_scrollView removeObserver:self forKeyPath:@"contentInset"];
-    _scrollView.delegate = _realDelegate;
+    [_scrollView removeObserver:self forKeyPath:@"contentOffset"];
+    [_scrollView removeObserver:self forKeyPath:@"contentInset"];
 }
 
 - (void)positionRefreshView
@@ -89,43 +80,85 @@ static void * KVOContext = &KVOContext;
         }
     }
 }
-//
-//- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-//{
-//    // if it's this class instance that's doing the observation
-//    if (context == KVOContext) {
-//        DLog(@"scrolliew %@ inset changed:%@", object, change);
-//        if ([keyPath isEqualToString:@"contentInset"]) {
-//            // reposition the refresh view when view controller automatically sets insets for our scroll view
-//            if (! _ignoreInsetChanges) {
-//                _originalInsets = [[change objectForKey:@"new"] UIEdgeInsetsValue];
-//                [self positionRefreshView];
-//                _ignoreInsetChanges = YES;
-//            }
-//        }
-//    }
-//}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    // if it's this class instance that's doing the observation
+    if (context == KVOContext) {
+        if ([keyPath isEqualToString:@"contentInset"]) {
+            DLog(@"scrolliew %@ inset changed:%@", object, change);
+            // reposition the refresh view when view controller automatically sets insets for our scroll view
+            if (! _ignoreInsetChanges) {
+                _originalInsets = [[change objectForKey:@"new"] UIEdgeInsetsValue];
+                [self positionRefreshView];
+            }
+       } else if ([keyPath isEqualToString:@"contentOffset"]) {
+           // if we are just starting the movement
+           if (self.state == RHRefreshStateHidden && _scrollView.dragging) {
+               [self setState:RHRefreshStateNormal];
+           }
+           
+           [self positionRefreshView];
+           
+           if (self.state == RHRefreshStateLoading) {
+               if (! _scrollView.dragging && ! _insetSet) {
+                   DLog(@"We ended dragging and it's loading state, so we add the inset");
+                   _insetSet = YES;
+                   
+                   _ignoreInsetChanges = YES;
+                   [UIView animateWithDuration:0.2 animations:^{
+                       _scrollView.contentInset = UIEdgeInsetsMake(self.minimumForStart + _originalInsets.top, 0.0f, 0.0f, 0.0f);
+                   } completion:^(BOOL finished) {
+                       _ignoreInsetChanges = NO;
+                   }];
+               }
+           } else if (self.state != RHRefreshStateHidden) { // if it's hidden then user has released the scrollview and we're closing it, no point to update here
+               // if we have pulled enough then start loading phase
+               if (_scrollView.contentOffset.y + _originalInsets.top + self.minimumForStart <= -self.maximumForPull) {
+                   // the state at this point can only be Normal or Pulling
+                   if (self.state != RHRefreshStateLoading) {
+                       [self setState:RHRefreshStateLoading];
+                   }
+               } else if (_scrollView.contentOffset.y + _originalInsets.top + self.minimumForStart < 0) { // if we have pulled more than a minimum
+                   if (self.state != RHRefreshStatePulling) {
+                       [self setState:RHRefreshStatePulling];
+                   }
+                   
+                   CGFloat deltaOffsetY = MIN(fabsf(_scrollView.contentOffset.y + _originalInsets.top + self.minimumForStart), self.maximumForPull);
+                   CGFloat percentage = deltaOffsetY / self.maximumForPull;
+                   
+                   [self.refreshView updateViewWithPercentage:percentage state:self.state];
+               } else {
+                   [self.refreshView updateViewWithPercentage:0 state:self.state];
+               }
+           }
+       }
+    }
+}
 
 - (void)beginRefreshing
 {
     if (self.state != RHRefreshStateLoading) {
         [self setState:RHRefreshStateLoading];
-        [UIView animateWithDuration:0.3 animations:^{
-            _scrollView.contentOffset = CGPointMake(_scrollView.contentOffset.x, - (_originalInsets.top + self.minimumForStart));
-        }];
+        [_scrollView setContentOffset:CGPointMake(_scrollView.contentOffset.x,  - (_originalInsets.top + self.minimumForStart)) animated:YES];
     }
 }
 
 - (void)endRefreshing {
     if (_scrollView && self.state != RHRefreshStateHidden) {
+        // set this first so that it's not loading and we wouldn't try to add inset in KVO method
+        [self setState:RHRefreshStateHidden];
         
         DLog(@"ActuallyEndLoading - Set scrollview insets to %f", _originalInsets.top);
         
         
         // if user holded the scrollview for the whole time, we won't have the inset, so we reset it only if we do have it
         if (_scrollView.contentInset.top != _originalInsets.top) {
+            _ignoreInsetChanges = YES;
             [UIView animateWithDuration:0.2 animations:^{
                 [_scrollView setContentInset:_originalInsets];
+            } completion:^(BOOL finished) {
+                _ignoreInsetChanges = NO;
             }];
         }
         
@@ -143,7 +176,6 @@ static void * KVOContext = &KVOContext;
             [self.refreshView updateViewOnComplete];
         }
         
-        [self setState:RHRefreshStateHidden];
     } else {
         DLog(@"end loading called, but refresh control wasn't active");
     }
@@ -163,6 +195,7 @@ static void * KVOContext = &KVOContext;
             if ([_delegate respondsToSelector:@selector(refreshDidTriggerRefresh:)]) {
                 [_delegate refreshDidTriggerRefresh:self];
             }
+            _insetSet = NO;
             break;
             
         case RHRefreshStatePulling:
@@ -182,112 +215,6 @@ static void * KVOContext = &KVOContext;
 }
 
 
-
-#pragma mark - UIScrollViewDelegate
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewDidScroll:)]) {
-        [_realDelegate scrollViewDidScroll:scrollView];
-    }
-    
-    // if we are just starting the movement
-    if (self.state == RHRefreshStateHidden && _scrollView.dragging) {
-        [self setState:RHRefreshStateNormal];
-    }
-    
-    [self positionRefreshView];
-    
-    if (self.state != RHRefreshStateHidden) { // if it's hidden then user has released the scrollview and we're closing it, no point to update here
-        // if we have pulled enough then start loading phase
-        if (_scrollView.contentOffset.y + _originalInsets.top + self.minimumForStart <= -self.maximumForPull) {
-            // the state at this point can only be Normal or Pulling
-            if (self.state != RHRefreshStateLoading) {
-                [self setState:RHRefreshStateLoading];
-            }
-        } else if (_scrollView.contentOffset.y + _originalInsets.top + self.minimumForStart < 0) { // if we have pulled more than a minimum
-            if (self.state != RHRefreshStatePulling) {
-                [self setState:RHRefreshStatePulling];
-            }
-            
-            CGFloat deltaOffsetY = MIN(fabsf(_scrollView.contentOffset.y + _originalInsets.top + self.minimumForStart), self.maximumForPull);
-            CGFloat percentage = deltaOffsetY / self.maximumForPull;
-            
-            [self.refreshView updateViewWithPercentage:percentage state:self.state];
-        } else {
-            [self.refreshView updateViewWithPercentage:0 state:self.state];
-        }
-    }
-}
-
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewWillBeginDragging:)]) {
-        [_realDelegate scrollViewWillBeginDragging:scrollView];
-    }
-}
-
-- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewWillEndDragging:withVelocity:targetContentOffset:)]) {
-        [_realDelegate scrollViewWillEndDragging:scrollView withVelocity:velocity targetContentOffset:targetContentOffset];
-    }
-}
-
-- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewDidEndDragging:willDecelerate:)]) {
-        [_realDelegate scrollViewDidEndDragging:scrollView willDecelerate:decelerate];
-    }
-
-    if (self.state == RHRefreshStateLoading) {
-        DLog(@"We ended dragging and it's loading state, so we add the inset");
-        [UIView animateWithDuration:.1 animations:^{
-            _scrollView.contentInset = UIEdgeInsetsMake(self.minimumForStart + _originalInsets.top, 0.0f, 0.0f, 0.0f);
-        }];
-    }
-}
-
-- (BOOL)scrollViewShouldScrollToTop:(UIScrollView *)scrollView
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewShouldScrollToTop:)]) {
-        return [_realDelegate scrollViewShouldScrollToTop:scrollView];
-    } else {
-        return YES; //it's default without the delegate
-    }
-}
-
-- (void)scrollViewDidScrollToTop:(UIScrollView *)scrollView
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewDidScrollToTop:)]) {
-        [_realDelegate scrollViewDidScrollToTop:scrollView];
-    }
-}
-
-- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewWillBeginDecelerating:)]) {
-        [_realDelegate scrollViewWillBeginDecelerating:scrollView];
-    }
-}
-
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(scrollViewDidEndDecelerating:)]) {
-        [_realDelegate scrollViewDidEndDecelerating:scrollView];
-    }
-}
-
-#pragma mark - UITableViewDelegate
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (_realDelegate && [_realDelegate respondsToSelector:@selector(tableView:heightForRowAtIndexPath:)]) {
-        return [(id<UITableViewDelegate>)_realDelegate tableView:tableView heightForRowAtIndexPath:indexPath];
-    } else {
-        return 0;
-    }
-}
 
 
 @end
